2f29bcb44378ce5ef2275f55c522a15af6de0471
GHaxZ <ghax.z.yt@proton.me>
2024-06-12 17:30:04 +0200

    Fixed some issues in .bashrc and update.sh

--- private_dot_local/private_bin/executable_update.sh
+++ private_dot_local/private_bin/executable_update.sh
@@ -2,63 +2,64 @@
 
 # Check if script is run as sudo
 if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root (sudo)." 1>&2
-    exit 1
+	echo "This script must be run as root (sudo)." 1>&2
+	exit 1
 fi
 
 usage() {
-    echo "Usage: sudo $0 [-y] [-r]" 1>&2
-    echo "  -y  Automatically confirm updates" 1>&2
-    echo "  -r  Reboot after updates" 1>&2
-    echo "  -s  Reboot after updates" 1>&2
-    echo "  --help  Display this help message" 1>&2
-    exit 1
+	echo "Usage: sudo $0 [-y] [-r]" 1>&2
+	echo "  -y  Automatically confirm updates" 1>&2
+	echo "  -r  Reboot after updates" 1>&2
+	echo "  -s  Shutdown after updates" 1>&2
+	echo "  --help  Display this help message" 1>&2
+	exit 1
 }
 
 while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-        -y )
-            YES_FLAG=true
-            ;;
-        -r )
-            REBOOT_FLAG=true
-            ;;
-        -s )
-            SHUTDOWN_FLAG=true
-            ;;
-        --help )
-            usage
-            ;;
-        * )
-            echo "Invalid option: $1" 1>&2
-            usage
-            ;;
-    esac
-    shift
+	case "$1" in
+	-y)
+		YES_FLAG=true
+		;;
+	-r)
+		REBOOT_FLAG=true
+		;;
+	-s)
+		SHUTDOWN_FLAG=true
+		;;
+	--help)
+		usage
+		;;
+	*)
+		echo "Invalid option: $1" 1>&2
+		usage
+		;;
+	esac
+	shift
 done
 
 echo "Running system update..."
 if [ "$YES_FLAG" = true ]; then
-    sudo zypper dup -y
+	sudo zypper dup -y
 else
-    sudo zypper dup
+	sudo zypper dup
 fi
 
 echo "Updating flatpaks..."
 if [ "$YES_FLAG" = true ]; then
-    sudo flatpak update -y
+	sudo flatpak update -y
 else
-    sudo flatpak update
+	sudo flatpak update
 fi
 
 echo "Update process complete."
 
 if [ "$REBOOT_FLAG" = true ]; then
-    echo "Rebooting system..."
-    sudo shutdown -r now
+	echo "Rebooting system..."
+	sudo shutdown -r now
 fi
 
 if [ "$SHUTDOWN_FLAG" = true ]; then
-    echo "Shutting down system..."
-    sudo shutdown now
+	echo "Shutting down system..."
+	sudo shutdown now
 fi
+
