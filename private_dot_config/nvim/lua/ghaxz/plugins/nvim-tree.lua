86bf6121c7ab3e99a9106e41d3de13de76dfac43
GHaxZ <ghax.z.yt@proton.me>
2024-06-20 00:09:43 +0200

    Added nvim-tree, added leap (not fully configured yet), removed substitute

---
+++ private_dot_config/nvim/lua/ghaxz/plugins/nvim-tree.lua
@@ -0,0 +1,40 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+
+	config = function()
+		local nvimtree = require("nvim-tree")
+
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
+
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+			},
+
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+
+			git = {
+				ignore = false,
+			},
+		})
+
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+	end,
+}
